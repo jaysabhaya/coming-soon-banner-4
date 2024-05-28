3bfbbf6e55a68eedcebeb9f423dfc9580e09c196
ravigadhiyaflexion <139459159+ravigadhiyaflexion@users.noreply.github.com>
2024-05-28 15:07:18 +0530

    updated file

--- coming-soon-banner-4/vite.config.js
+++ coming-soon-banner-4/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/coming-soon-banner-4"
+  // base: "/coming-soon-banner-4"
 })
